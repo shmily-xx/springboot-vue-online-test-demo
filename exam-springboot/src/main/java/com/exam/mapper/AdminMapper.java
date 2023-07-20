c8e9ae106a7b48114610cc0eeac059b66800a104
shmily-xx <fw521999@outlook.com>
2023-07-20 14:28:42 +0800

    first commit

---
+++ exam-springboot/src/main/java/com/exam/mapper/AdminMapper.java
@@ -0,0 +1,30 @@
+package com.exam.mapper;
+
+import com.exam.entity.Admin;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface AdminMapper {
+
+    //    @Select("select adminName,sex,tel,email,cardId,role from admin")
+    List<Admin> findAll();
+
+    //    @Select("select adminName,sex,tel,email,cardId,role from admin where adminId = #{adminId}")
+    Admin findById(Integer adminId);
+
+    //    @Delete("delete from admin where adminId = #{adminId}")
+    int deleteById(int adminId);
+
+    //    @Update("update admin set adminName = #{adminName},sex = #{sex}," +
+//            "tel = #{tel}, email = #{email},pwd = #{pwd},cardId = #{cardId},role = #{role} where adminId = #{adminId}")
+    int update(Admin admin);
+
+    //    @Options(useGeneratedKeys = true,keyProperty = "adminId")
+//    @Insert("insert into admin(adminName,sex,tel,email,pwd,cardId,role) " +
+//            "values(#{adminName},#{sex},#{tel},#{email},#{pwd},#{cardId},#{role})")
+    int add(Admin admin);
+}
