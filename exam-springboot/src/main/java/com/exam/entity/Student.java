fb7b90ba9d1459cc99a2ccbb1fd1f8de63be1507
shmily-xx <fw521999@outlook.com>
2023-08-27 19:21:18 +0800

    加上实体字段注释

--- exam-springboot/src/main/java/com/exam/entity/Student.java
+++ exam-springboot/src/main/java/com/exam/entity/Student.java
@@ -1,5 +1,8 @@
 package com.exam.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
@@ -10,41 +13,53 @@ import javax.validation.constraints.NotNull;
 @Data
 public class Student {
 
+    @TableId(value = "teacherId",type = IdType.AUTO)//在自增主键的变量加上即可
     private Integer studentId;
 
     /**
      * 名字
      */
     @NotNull
+    @ApiModelProperty("名字")
     private String studentName;
 
     @NotNull
+    @ApiModelProperty("年级")
     private String grade;
 
     @NotNull
+    @ApiModelProperty("专业")
     private String major;
 
     @NotNull
+    @ApiModelProperty("班级")
     private String clazz;
 
     @NotNull
+    @ApiModelProperty("学院")
     private String institute;
 
     @NotNull
+    @ApiModelProperty("电话号码")
     private String tel;
 
     @NotNull
+    @ApiModelProperty("email")
     private String email;
 
     @NotNull
+    @ApiModelProperty("密码")
     private String pwd;
 
     @NotNull
+    @ApiModelProperty("身份证号")
     private String cardId;
 
     @NotNull
+    @ApiModelProperty("性别")
     private String sex;
 
     @NotNull
+    @ApiModelProperty("身份")
     private String role;
 }
--- exam-springboot/src/main/java/com/exam/entity/Teacher.java
+++ exam-springboot/src/main/java/com/exam/entity/Teacher.java
@@ -1,5 +1,8 @@
 package com.exam.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
@@ -9,32 +12,43 @@ import javax.validation.constraints.NotNull;
  */
 @Data
 public class Teacher {
+
+    @TableId(value = "teacherId",type = IdType.AUTO)//在自增主键的变量加上即可
     private Integer teacherId;
 
     @NotNull
+    @ApiModelProperty("名字")
     private String teacherName;
 
     @NotNull
+    @ApiModelProperty("所属学院")
     private String institute;
 
     @NotNull
+    @ApiModelProperty("性别")
     private String sex;
 
     @NotNull
+    @ApiModelProperty("电话号码")
     private String tel;
 
     @NotNull
+    @ApiModelProperty("email")
     private String email;
 
     @NotNull
+    @ApiModelProperty("密码")
     private String pwd;
 
     @NotNull
+    @ApiModelProperty("身份证号")
     private String cardId;
 
     @NotNull
+    @ApiModelProperty("职级")
     private String type;
 
     @NotNull
+    @ApiModelProperty("身份")
     private String role;
 }
